c0582fd911e96525b769a2ae201312936ddb8827
David Barri <japgolly@gmail.com>
2016-12-16 08:58:52 +1100

    cp

--- prop/shared/src/main/scala/nyaya/neo/Neo2.scala
+++ prop/shared/src/main/scala/nyaya/neo/Neo2.scala
@@ -44,37 +44,43 @@ Eval should be called...
 * Test
  */
 
+  /**
+    * @tparam A Result
+    * @tparam R Recursive case
+    */
   sealed trait LogicF[+A, +R]
   object LogicF {
     case class Value[+A](value: A) extends LogicF[A, Nothing]
-    case class Conjunction[+R](a: R, b: R, c: List[R]) extends LogicF[Nothing, R]
-    case class Disjunction[+R](a: R, b: R, c: List[R]) extends LogicF[Nothing, R]
+    case class Conjunction[+R](a: R, b: R, c: List[R]) extends LogicF[Nothing, R] {
+      def conjuncts: List[R] = a :: b :: c
+    }
+    case class Disjunction[+R](a: R, b: R, c: List[R]) extends LogicF[Nothing, R] {
+      def disjuncts: List[R] = a :: b :: c
+    }
     case class Negation[+R](a: R) extends LogicF[Nothing, R]
     case class Implication[+R](a: R, b: R) extends LogicF[Nothing, R]
     case class Biconditional[+R](a: R, b: R) extends LogicF[Nothing, R]
   }
 
-  case class Show[A](show: A => String) extends AnyVal
-  trait Failure {
-    type I
-    val input: I
-    val show: Show[I]
-    val failure: String
-  }
+//  case class Show[A](show: A => String) extends AnyVal
+//  trait EvalInput { // TODO This seems wrong. Every Eval has an input?
+//    type I
+//    val input: I
+//    val show: Show[I]
+//  }
+  // TODO In prop tests we need to show the random value used in failure
+  case class Failure(reason: String)
   case class EvalN(name: Name, failure: Option[Failure])
   type EvalF[+R] = LogicF[EvalN, R]
   type Eval = Fix[EvalF]
 
-  case class PropN[-A](name: Name, prop: A => Eval)
+  case class PropN[-A](nameFn: NameFn[A], prop: A => Eval)
   type PropF[A, +R] = LogicF[PropN[A], R]
   type Prop[A] = Fix[PropF[A, ?]]
 
+  // ===================================================================================================================
 
   type TraceTree = Eval
-
-//  type ConclusionB = Boolean
-//  type ConclusionFirstFailure = Option[Failure]
-//  type ConclusionFailures = List[Failure]
 
   sealed trait FailureTreeF[+R]
   object FailureTreeF {
@@ -82,13 +88,25 @@ Eval should be called...
     case class Branch[+R](children: NonEmptyVector[R]) extends FailureTreeF[R]
   }
   type FailureTree = Fix[FailureTreeF]
+  object FailureTree {
+    def leaf(f: Failure): FailureTree = Fix[FailureTreeF](FailureTreeF.Leaf(f))
+    def branch(f: NonEmptyVector[Fix[FailureTreeF]]): FailureTree = Fix[FailureTreeF](FailureTreeF.Branch(f))
+  }
 
   // TODO metamorphism
   // prune branches
 
   def traceToFailureTree: Algebra[EvalF, Option[FailureTree]] = {
     case LogicF.Value(EvalN(_, None)) => None
-    case LogicF.Value(EvalN(n, Some(f))) => Some(Fix[FailureTreeF](FailureTreeF.Leaf(f)))
+    case LogicF.Value(EvalN(n, Some(f))) => Some(FailureTree.leaf(f))
+    case LogicF.Negation(Some(ft)) => None
+//    case LogicF.Negation(None) => Some(FailureTree.leaf( ?????? ))
+
+    case l: LogicF.Conjunction[Option[FailureTree]] =>
+      NonEmptyVector.option(l.conjuncts.toIterator.filterDefined.toVector)
+        .map(FailureTree.branch)
+
+    // TODO Biconditional(_, _), Disjunction(_, _, _), Implication(_, _)
   }
 
   // Algebra[EvalF, Option FT]
