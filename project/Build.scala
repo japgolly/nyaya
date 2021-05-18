926b6416293d1b116f3f679342b30747a1e03eff
Scala Steward <me@scala-steward.org>
2021-05-18 08:31:15 +0200

    Update scala-library to 2.13.6

--- project/Build.scala
+++ project/Build.scala
@@ -24,7 +24,7 @@ object NyayaBuild {
     val Monocle          = "1.6.3"
     val MTest            = "0.7.10"
     val Scala212         = "2.12.13"
-    val Scala213         = "2.13.5"
+    val Scala213         = "2.13.6"
     val Scala3           = "3.0.0"
     val ScalaCollCompat  = "2.4.4"
     val Scalaz           = "7.2.31"
