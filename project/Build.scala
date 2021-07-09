34ba9fd3a677ac20a1dbb285a03349ddf1565528
Scala Steward <me@scala-steward.org>
2021-07-09 11:11:52 +0200

    Update scala-collection-compat to 2.5.0

--- project/Build.scala
+++ project/Build.scala
@@ -26,7 +26,7 @@ object NyayaBuild {
     val Scala212         = "2.12.13"
     val Scala213         = "2.13.5"
     val Scala3           = "3.0.0"
-    val ScalaCollCompat  = "2.4.4"
+    val ScalaCollCompat  = "2.5.0"
     val Scalaz           = "7.2.31"
   }
 
